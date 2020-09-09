d66387f6d06ee4a03f2428342f13ca266cc542f7
sarahduv <sarah.duvdevani@gmail.com>
2020-09-08 20:23:50 -0700

    conversions are working and are rounded

--- app/src/main/java/com/example/temperatureconversion/FirstFragment.kt
+++ app/src/main/java/com/example/temperatureconversion/FirstFragment.kt
@@ -1,12 +1,18 @@
 package com.example.temperatureconversion
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_first.*
+import java.lang.Exception
+import kotlin.math.roundToLong
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -23,9 +29,38 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        temp_input.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
 
-        view.findViewById<Button>(R.id.kelvin_button).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+            }
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+
+                val input = s.toString()
+
+                try {
+                    val f = input.toDouble()
+
+                    val c = ((f-32) * (5.0/9.0))
+                    val cRounded = (c*100).roundToLong()/100.0
+                    celsius_update.text = cRounded.toString()
+
+                    val k = c + 273.15
+                    val kRounded = (k*100).roundToLong()/100.0
+                    kelvin_update.text = kRounded.toString()
+
+                }
+                catch(e: Exception) {
+                    // value is invalid
+                    celsius_update.text = ""
+                    kelvin_update.text = ""
+                }
+
+            }
+
+            override fun afterTextChanged(s: Editable) {
+
+            }
+        })
+
     }
 }
--- app/src/main/java/com/example/temperatureconversion/MainActivity.kt
+++ app/src/main/java/com/example/temperatureconversion/MainActivity.kt
@@ -14,10 +14,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -26,13 +22,4 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
 }
